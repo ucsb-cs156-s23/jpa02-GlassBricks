c2f1d4a00ed421c5179151c8e08134c1e3403788
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2023-04-19 19:04:20 -0700

    by - change page message

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -1,14 +1,14 @@
 package edu.ucsb.cs156.spring.hello;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from benjamin-ye!";
     }
-    
+
 }
